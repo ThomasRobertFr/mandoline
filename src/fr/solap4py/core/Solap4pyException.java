1e0c4231fb5447639e55f4883cdfb6bd23dbcc9f
Rémy Chevalier <remychevalier@laposte.net>
2014-04-15 08:20:13 +0200

    added java integration tests

--- src/fr/solap4py/core/Solap4pyException.java
+++ src/fr/solap4py/core/Solap4pyException.java
@@ -1,13 +1,7 @@
 package fr.solap4py.core;
 
-
 enum ErrorType {
-    BAD_REQUEST,
-    NOT_SUPPORTED,
-    SERVER_ERROR,
-    NO_HIERARCHY,
-    DIMENSION_ID_COUNT,
-    UNKNOWN_ERROR
+    BAD_REQUEST, NOT_SUPPORTED, SERVER_ERROR, NO_HIERARCHY, DIMENSION_ID_COUNT, UNKNOWN_ERROR
 };
 
 @SuppressWarnings("serial")
@@ -17,25 +11,26 @@ public class Solap4pyException extends Exception {
     private final ErrorType TYPE;
 
     public Solap4pyException(ErrorType type, String description) {
-        super(description);
-        this.TYPE = type;
-        this.DESCRIPTION = description == null ? "null" : description;
+	super(description);
+	this.TYPE = type;
+	this.DESCRIPTION = description == null ? "null" : description;
 
     }
 
     public Solap4pyException(ErrorType type, Exception exception) {
-        this(type, exception.getMessage());
+	this(type, exception.getMessage());
     }
 
-//    public JSONObject getJSON() throws JSONException {
-//        JSONObject objectJson = new JSONObject();
-//        objectJson.put("error", TYPE.toString());
-//        objectJson.put("data", DESCRIPTION);
-//        return objectJson;
-//    }
-    
+    // public JSONObject getJSON() throws JSONException {
+    // JSONObject objectJson = new JSONObject();
+    // objectJson.put("error", TYPE.toString());
+    // objectJson.put("data", DESCRIPTION);
+    // return objectJson;
+    // }
+
     public String getJSON() {
-	return "{ \"error\": \"" + TYPE.toString() + "\", \"data\": \"" + DESCRIPTION + "\"}"; 
+	return "{ \"error\": \"" + TYPE.toString() + "\", \"data\": \""
+		+ DESCRIPTION + "\"}";
     }
 
 }
