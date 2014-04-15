c8f7cef5a1417d36ab7cca21bf722a38e3a65976
croullet <cindy.roullet@insa-rouen.fr>
2014-04-15 09:58:27 +0200

    Format of AllTests class rectified

--- test/fr/solap4py/core/AllTests.java
+++ test/fr/solap4py/core/AllTests.java
@@ -10,20 +10,15 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ IntegrationTest.class, JSONBuilderTest.class,
 		MDXBuilderTest.class, MetadataTest.class, Solap4pyTest.class })
 public class AllTests {
-	 // Execution begins at main().  In this test class, we will execute
-    // a text test runner that will tell you if any of your tests fail.
-    public static void main (String[] args) 
-    {
-       junit.textui.TestRunner.run (suite());
-    }
+	// Execution begins at main(). In this test class, we will execute
+	// a text test runner that will tell you if any of your tests fail.
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
 
-    // The suite() method is helpful when using JUnit 3 Test Runners or Ant.
-    public static junit.framework.Test suite() 
-    {
-       return new JUnit4TestAdapter(AllTests.class);
-    }
-	
-	
-	
+	// The suite() method is helpful when using JUnit 3 Test Runners or Ant.
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(AllTests.class);
+	}
+
 }
-
