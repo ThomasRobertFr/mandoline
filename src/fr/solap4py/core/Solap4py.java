c9b8747e9920a1a4025f791d93a54b077b447fac
Rémy Chevalier <remychevalier@laposte.net>
2014-05-05 09:33:43 +0200

    Changed listening port. Use getSolap4Object function instead of constructor to retrieve solap4py object.

--- src/fr/solap4py/core/Solap4py.java
+++ src/fr/solap4py/core/Solap4py.java
@@ -1,10 +1,14 @@
 package fr.solap4py.core;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -57,13 +61,13 @@ public class Solap4py {
      *            the input query in JSON format
      * @return the result of the query in JSON format
      */
-    public byte[] process(byte[] byteQuery) {
+    public String process(String query) {
         String result;
-        long startTime = System.nanoTime();
+//        long startTime = System.nanoTime();
 
-        String query = new String(byteQuery, StandardCharsets.UTF_8);
-        System.out.println(query);
-        System.out.println("Time for converting input : " + (System.nanoTime() - startTime));
+//        String query = new String(byteQuery, StandardCharsets.UTF_8);
+//        System.out.println(query);
+//        System.out.println("Time for converting input : " + (System.nanoTime() - startTime));
         try {
             try {
                 /* Here, we process the query */
@@ -90,11 +94,12 @@ public class Solap4py {
             result = se.getJSON();
         }
         
-        System.out.println("Process time : " + (System.nanoTime() - startTime));
-        byte[] byteResult = result.getBytes(StandardCharsets.UTF_8);
-        System.out.println("Time for converting result : " + (System.nanoTime() - startTime));
+//        System.out.println("Process time : " + (System.nanoTime() - startTime));
+//        byte[] byteResult = result.getBytes(StandardCharsets.UTF_8);
+//        System.out.println("Time for converting result : " + (System.nanoTime() - startTime));
 
-        return byteResult;
+//        return byteResult;
+        return result;
     }
 
     /**
@@ -173,15 +178,9 @@ public class Solap4py {
     }
     
     
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        if (args.length() != 4) {
-            System.err.println("bla bla bla");
-            System.exit(1);
-        }
-        
-        Solap4py solap4py = new Solap4Py(args[0], args[1], args[2], args[3]);
-        ServerSocket server = new ServerSocket(5000); // TODO which port should we use ?
-        
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        final Solap4py solap4py = Solap4py.getSolap4Object();
+        ServerSocket server = new ServerSocket(25335); // TODO which port should we use ?
         while (true) {
             final Socket client = server.accept();
             new Thread() {
@@ -190,10 +189,8 @@ public class Solap4py {
                     try {
                         BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                         PrintWriter out = new PrintWriter(client.getOutputStream());
-                        
                         String query = in.readLine();
                         String result = solap4py.process(query);
-                        
                         out.print(result);
                         out.flush();
                         client.close();
@@ -201,7 +198,7 @@ public class Solap4py {
                         e.printStackTrace();
                     }
                 }
-            }
+            };
         }
     }
 }
