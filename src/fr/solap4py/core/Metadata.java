78750c130c92e2d451ae9056a7b4292e72ccf837
Rémy Chevalier <remychevalier@laposte.net>
2014-05-12 09:37:13 +0200

    inexistant granularity in metadata throws an exception

--- src/fr/solap4py/core/Metadata.java
+++ src/fr/solap4py/core/Metadata.java
@@ -328,6 +328,10 @@ public class Metadata {
             Dimension dimension = this.extractDimension(from, cube);
             Hierarchy hierarchy = this.extractHierarchy(from, dimension);
             Level level = this.extractLevel(from, hierarchy);
+            int depth = level.getDepth() + granularity;
+            if (depth >= hierarchy.getLevels().size()) {
+        	throw new Solap4pyException(ErrorType.BAD_REQUEST, "Inexistant granularity");
+            }
             
             String current = null;
             List<Member> tmp = level.getMembers();
@@ -358,7 +362,6 @@ public class Metadata {
               		}
               	}
               	else{
-
               		for (Member member : members) {              	
 	                    if (member.getUniqueName().equals(from.getString(5))) {
 	                        List<Member> memberArray = new LinkedList<Member>(Arrays.asList(member));
@@ -550,12 +553,9 @@ public class Metadata {
     
     
     public static void main(String[] args) throws ClassNotFoundException, SQLException, JSONException {
-
-
-
-        String param = " { \"root\" : [\"Traffic\", \"[Traffic]\", \"[Zone]\", \"[Zone.Name]\", \"[Zone.Name].[Name0]\", [\"[Zone.Name].[All Zone.Names].[France]\" , \"[Zone.Name].[All Zone.Names].[Spain]\" ] ], \"withProperties\":false, \"granularity\":0}}";
+	
+        String param = " { \"root\" : [\"Traffic\", \"[Traffic]\", \"[Zone]\", \"[Zone.Name]\", \"[Zone.Name].[Name0]\", \"[Zone.Name].[All Zone.Names].[France]\"], \"withProperties\":false, \"granularity\":3}}";
         Solap4py p = Solap4py.getSolap4Object();
-
         JSONObject query = new JSONObject(param);
         Metadata m = new Metadata(p.getOlapConnection());
         JSONObject result = new JSONObject();
